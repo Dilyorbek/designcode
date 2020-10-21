42d35cd83392bb5f39f2d4597eab546870f01bce
Dilyorbek <dilbek1993@gmail.com>
2020-10-21 21:03:52 +0200

    bottom panel added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:designcode/components/lists/recent_course_list.dart';
-import 'package:designcode/constants.dart';
-import 'package:designcode/screens/sidebar_screen.dart';
+import 'package:designcode/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'components/home_screen_nav_bar.dart';
-import 'components/lists/explore_course_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,109 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeWidget(),
-    );
-  }
-}
-
-class HomeWidget extends StatefulWidget {
-  const HomeWidget({Key key}) : super(key: key);
-
-  @override
-  _HomeWidgetState createState() => _HomeWidgetState();
-}
-
-class _HomeWidgetState extends State<HomeWidget> with TickerProviderStateMixin {
-  Animation<Offset> sidebarAnimation;
-  AnimationController sidebarAnimationCoontroller;
-
-  @override
-  void initState() {
-    super.initState();
-    sidebarAnimationCoontroller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 250),
-    );
-
-    sidebarAnimation = Tween<Offset>(
-      begin: Offset(-1, 0),
-      end: Offset(0, 0),
-    ).animate(
-      CurvedAnimation(
-        parent: sidebarAnimationCoontroller,
-        curve: Curves.easeInOut
-      )
-    );
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: kBackgroundColor,
-        child: Stack(
-          children: [
-            SafeArea(
-              child: Column(
-                children: [
-                  HomeScreenNavBar(toggleAnimation: (){
-                    sidebarAnimationCoontroller.forward();
-                  },),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          "Recents",
-                          style: kLargeTitleStyle,
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Text(
-                          "23 courses, more coming",
-                          style: kSubtitleStyle,
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  RecentCourseList(),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 20.0,
-                      right: 20.0,
-                      top: 25.0,
-                      bottom: 16.0,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          "Explore",
-                          style: kTitle1Style,
-                        ),
-                      ],
-                    ),
-                  ),
-                  ExploreCourseList(),
-                ],
-              ),
-            ),
-            SlideTransition(
-              position: sidebarAnimation,
-              child: SafeArea(
-                child: SidebarScreen(),
-                bottom: false,
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
